abd92ff8a9457b97e8599a5cd976660b7060e373
Loris Santamaria <loris@lgs.com.ve>
2016-05-09 21:44:15 -0400

    Style cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -63,72 +63,74 @@
 # Copyright 2016 Loris Santamaria.
 #
 class ipaclient (
-	$realm          = "REALM.COM",
-	$domain         = "dnsdomain.com",
-	$trustedrealm   = "AD.REALM.COM",
-	$trusteddomain  = "ad.dnsdomain.com",
-	$k5loginusers   = "admin@REALM.COM",
-	$suffix         = "dc=example,dc=com",
-	$ldapservers    = "myldapserver.dnsdomain.com",
-    $sudobindpw     = "eituyuiijjfdhfvjliooiutgy678",
-	$is_ipa_server  = "no"
+  $realm          = 'REALM.COM',
+  $domain         = 'dnsdomain.com',
+  $trustedrealm   = 'AD.REALM.COM',
+  $trusteddomain  = 'ad.dnsdomain.com',
+  $k5loginusers   = 'admin@REALM.COM',
+  $suffix         = 'dc=example,dc=com',
+  $ldapservers    = 'myldapserver.dnsdomain.com',
+  $sudobindpw     = 'eituyuiijjfdhfvjliooiutgy678',
+  $is_ipa_server  = 'no'
 ){
-    package {
-        "ipa-client":
-            ensure => present;
-        "openldap-clients":
-            ensure => present;
-        "krb5-workstation":
-            ensure => present;
-        "cyrus-sasl-gssapi":
-            ensure => present;
-        "sssd":
-            ensure => present;
-        "sssd-dbus":
-            ensure => $lsbmajdistrelease ? {
-                "5"     => absent,
-                default => present,
-            };
-        "sudo":
-            ensure => present;
-    }
-    file { 
-	    "/etc/nss_ldap.conf":
-		    content => template('directorio/nss_ldap.conf.erb');
-	    "/etc/ldap.conf":
-		    ensure  => "/etc/nss_ldap.conf",
-		    require => File["/etc/nss_ldap.conf"];
-	    "/etc/openldap/ldap.conf":
-		    content => template('directorio/ldap.conf.erb'),
-	        require => Package["openldap-clients"];
-        "/etc/krb5.conf":
-	        content => template('directorio/krb5.conf.erb'),
-	        require => Package["krb5-workstation"];
-        "/etc/sssd/sssd.conf":
-	        content => template('directorio/sssd.conf.erb'),
-	        owner   => "root",
-	        group   => "root",
-	        mode    => "0600",
-	        require => [ Package["sssd"], Package["sssd-dbus"] ];
-        "/etc/nsswitch.conf":
-	        content => template('directorio/nsswitch.conf.erb'),
-	        require => Package["ipa-client"];
-        "/root/.k5login":
-	        mode    => "0600",
-	        seltype => "krb5_home_t",
-	        seluser => "unconfined_u",
-	        require => K5login["/root/.k5login"];
-    }
-	k5login {
-	    "/root/.k5login":
-	    	mode       => "600",
-	    	principals => [ $k5loginusers ];
-	}
-	service { 
-        "sssd":
-		    enable    => true,
-		    ensure    => running,
-		    require   => File["/etc/sssd/sssd.conf"],
-		    subscribe => File["/etc/sssd/sssd.conf"];
-	}
+  $has_sssd_dbus = $::lsbmajdistrelease ? {
+    '5'     => absent,
+    default => present,
+  }
+  package {
+    'ipa-client':
+        ensure => present;
+    'openldap-clients':
+        ensure => present;
+    'krb5-workstation':
+        ensure => present;
+    'cyrus-sasl-gssapi':
+        ensure => present;
+    'sssd':
+        ensure => present;
+    'sssd-dbus':
+        ensure => $has_sssd_dbus;
+    'sudo':
+        ensure => present;
+  }
+  file {
+    '/etc/nss_ldap.conf':
+        content => template('directorio/nss_ldap.conf.erb');
+    '/etc/ldap.conf':
+        ensure  => link,
+        target  => '/etc/nss_ldap.conf',
+        require => File['/etc/nss_ldap.conf'];
+    '/etc/openldap/ldap.conf':
+        content => template('directorio/ldap.conf.erb'),
+        require => Package['openldap-clients'];
+    '/etc/krb5.conf':
+        content => template('directorio/krb5.conf.erb'),
+        require => Package['krb5-workstation'];
+    '/etc/sssd/sssd.conf':
+        content => template('directorio/sssd.conf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0600',
+        require => [ Package['sssd'], Package['sssd-dbus'] ];
+    '/etc/nsswitch.conf':
+        content => template('directorio/nsswitch.conf.erb'),
+        require => Package['ipa-client'];
+    '/root/.k5login':
+        mode    => '0600',
+        seltype => 'krb5_home_t',
+        seluser => 'unconfined_u',
+        require => K5login['/root/.k5login'];
+  }
+  k5login {
+    '/root/.k5login':
+        mode       => '600',
+        principals => [ $k5loginusers ];
+  }
+  service {
+    'sssd':
+        ensure    => running,
+        enable    => true,
+        require   => File['/etc/sssd/sssd.conf'],
+        subscribe => File['/etc/sssd/sssd.conf'];
+  }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-describe 'freeipaclient' do
+describe 'ipaclient' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('freeipaclient') }
+    it { should contain_class('ipaclient') }
   end
 end
